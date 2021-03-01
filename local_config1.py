5d9a45799b0414be209341da65f63f1606ff5230
Elton <60447797+eltonLionel@users.noreply.github.com>
2021-03-01 18:24:11 -0500

    Add files via upload

---
+++ local_config1.py
@@ -0,0 +1,5 @@
+cons_tok = 'xxxxxxxxxxxxxxxxxxxxxxxx'
+cons_sec = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+app_tok = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+app_sec = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
